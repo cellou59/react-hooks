5fb5892e777601237929c6a216aafd264a66487b
cellouRetailShake <cellu@retailshake.com>
2024-04-06 09:31:11 +0200

    feat: 🎸 EmojiSearch component and fix translation issue

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,68 +2,43 @@
 // http://localhost:3000/alone/exercise/04.js
 // Fork de : https://github.com/ahfarmer/emoji-search/
 
-/* eslint-disable no-unused-vars */
 import * as React from 'react'
 import emojiList from '../emojiList'
 import Clipboard from 'clipboard'
 import '../04-styles.css'
 
-// 👨‍✈️ Créé l'application de recherche d'émojis selons les spécifications
-
-// 🐶 Nous allons traiter les composants un par un et pour rappel il faudra remonté les données
-// dans le composants principal <EmojiSearch />
-
 function Header({nbFound}) {
-  // 🐶 Fais en sorte que le Header affiche 'Aucun résultat' ou 'X emojis trouvés' en fonction de nbFound
   return (
     <div className="component-header">
       <div>Recherche Emoji</div>
       <div className="reusult-found">
-        {/* 🤖 Utilise une ternaire  */}
-        {/* nbFound > 0 ? 'OK' : 'KO'    */}
-        Aucun résultat
+        {nbFound > 0 ? `${nbFound} Emojis trouvés`: 'Aucun résultat'}
       </div>
     </div>
   )
 }
-// 🐶 Fais remonter la valeur saisie dans le champs input au composant parent.
-function SearchInput({onTextChange}) {
-  // 🐶 Crée une fonction onChange qui appelera la fonction onTextChange passée en props
-  // Rappel : `event.target.value` permet de récuperer la valeur de l'input.
-  // 🤖 onTextChange(event.target.value)
 
+function SearchInput({onTextChange}) {
   return (
     <div className="component-search-input">
       <div>
-        {/* 🐶 Gère l'évènement onChange sur le champs input*/}
-        {/* 🤖 onChange={onChange}*/}
-        <input />
+
+        <input onChange={e => onTextChange(e.target.value)}/>
       </div>
     </div>
   )
 }
 
-// 🐶 Gère le composant parent
 function EmojiSearch() {
-  // 🐶 Créé un state `dataEmoji` qui contiendra un tableau d'émojis
-  // 🤖 const [dataEmoji, setDataEmoji]
-
-  // 🐶 Créé une fonction 'handleTextChange' qui prend en paramètre 'text' le texte saisie dans le champs Input
-  // 🐶 Dans cette fonction, filtre les émojis avec la fonction `filterEmoji(text)`
-  // 🐶 Met à jour le state 'dataEmoji'(`setDataEmoji`) avec la liste filtrée d'émojis
-
-  // 🐶 Passe ensuite ces props aux composants enfants.
+   const [dataEmoji, setDataEmoji] = React.useState([])
+  const handleTextChange = text => {
+    setDataEmoji(filterEmoji(text))
+  }
   return (
     <div>
-      {/* 🐶 Passe le prop 'nbFound' au Header */}
-      {/* 🤖 utilise dataEmoji.length */}
-      <Header />
-      {/* 🐶 Passe le prop 'onTextChange' à  SearchInput */}
-      {/* 🤖 utilise dataEmoji.length */}
-      <SearchInput />
-      {/* 🐶 Passe le prop 'data' à Result */}
-      {/* 🤖 data={dataEmoji} */}
-      <Result />
+      <Header nbFound ={dataEmoji?.length}  />
+      <SearchInput dataEmoji={dataEmoji} onTextChange={handleTextChange} />
+      <Result data ={dataEmoji} />
     </div>
   )
 }
@@ -78,12 +53,10 @@ function EmojiSearch() {
 // 📑 il faut ensuite detruire l'objet quand on en a plus besoin
 // 🤖 clipboard.destroy();
 function Result({data = []}) {
-  // 🐶 Utilise 'useEffect' pour gérer l'instanciation de clipboard
-  // 🤖 React.useEffect
-  // 🤖 const clipboard = new Clipboard('.copy-to-clipboard')
-
-  // 🐶 N'oubllie pas de 'cleanup' detruire l'objet dans useEffect en retournant une fonction fléché
-  // 🤖 return () => { clipboard.destroy() }
+  React.useEffect (() => {
+    const clipboard = new Clipboard('.copy-to-clipboard')
+    return () =>  clipboard.destroy() 
+  }, [data])
   return (
     <div className="component-emoji-results">
       {data.map(emojiData => (
@@ -97,7 +70,7 @@ function Result({data = []}) {
   )
 }
 
-// 🐶 Gère la copie de l'emoji en appliquant lesattributs necessaires à clipboard
+// 🐶 Gère la copie de l'emoji en appliquant les attributs necessaires à clipboard
 function EmojiResultRow({symbol, title}) {
   // 🐶 Ajoute le className 'copy-to-clipboard'
   // 🤖 className="copy-to-clipboard"
@@ -105,7 +78,7 @@ function EmojiResultRow({symbol, title}) {
   // 🐶 Ajoute l'attribut data-clipboard-text à la div
   // 🤖 <div data-clipboard-text={symbol}
   return (
-    <div className="component-emoji-result-row">
+    <div className="component-emoji-result-row copy-to-clipboard" data-clipboard-text={symbol}>
       {symbol}
       <span className="title">{title}</span>
       <span className="info">Copier</span>
